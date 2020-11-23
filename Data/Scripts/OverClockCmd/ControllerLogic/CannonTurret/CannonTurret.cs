ac575d4d72926532006cee5f715a26eb9985e1f6
Lzywindy <lzywindy@163.com>
2020-11-23 18:48:25 +0800

    准备制作炮塔

---
+++ Data/Scripts/OverClockCmd/ControllerLogic/CannonTurret/CannonTurret.cs
@@ -0,0 +1,42 @@
+﻿using Sandbox.Game.Entities;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VRage.ModAPI;
+using VRageMath;
+
+namespace SuperBlocks
+{
+    public partial class CannonTurret : ControllerManageBase
+    {
+        public CannonTurret(IMyTerminalBlock refered_block) : base(refered_block) { }
+
+        protected override void Init(IMyTerminalBlock refered_block)
+        {
+            base.Init(refered_block);
+        }
+    }
+    public partial class CannonTurret
+    {
+        private const string 瞄准镜组ID = @"TargetFinder";
+        private IMyEntity Target;
+        private IMyTerminalBlock TargetLocker;
+        private Vector3 RelativePosition;
+        private double _Range;
+        public double Range { get { return _Range; } set { _Range = MathHelper.Clamp(value, 1e3, 1e6); } }
+        public void TriggerLockTarget()
+        {
+            Target = null;
+            if (TargetLocker == null) return;
+            VRage.Game.ModAPI.IHitInfo target = null;
+            bool HasEntity = MyAPIGateway.Physics.CastLongRay(TargetLocker.GetPosition(), TargetLocker.GetPosition() + TargetLocker.WorldMatrix.Forward * Range, out target, true);
+            if (!HasEntity || target == null) return;
+            if (target.HitEntity is IMyVoxelBase) return;
+            Target = target.HitEntity;
+            RelativePosition = Vector3.TransformNormal(target.Position - Target.GetPosition(), Matrix.Transpose(Target.WorldMatrix));
+        }
+    }
+}
